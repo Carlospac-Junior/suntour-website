36c6c011cfa00b977541b529849f3d5561fb2afe
Carlospac <carlospac.alberto@gmail.com>
2025-09-22 22:10:01 -0300

    Att - 22/09

--- assets/js/components/partials.js
+++ assets/js/components/partials.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const partials = [
+
+    // Principais partials
     { id: "header-container", file: "partials/header.html" },
     { id: "navbar-container", file: "partials/navbar.html" },
     { id: "footer-container", file: "partials/footer.html" },
+
+    // Pacotes
+    { id: "package-cancun-playa", file: "partials/pacotes/internacionais/cancunplayadelcamen.html" },
+    { id: "beto-carrero", file: "partials/pacotes/nacionais/betocarreroworld.html" },
+    { id: "joao-pessoa", file: "partials/pacotes/nacionais/joaopessoa.html" },
+    { id: "porto-de-galinhas", file: "partials/pacotes/nacionais/portodegalinhas.html" },
   ];
 
   partials.forEach(partial => {
@@ -16,3 +24,4 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 });
+
